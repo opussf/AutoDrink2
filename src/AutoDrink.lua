ca7ccf4e89a11286e3a38568e5812bf3f1d7e0b0
opussf <github@zz9-za.com>
2017-06-09 15:16:56 -0700

    Changes to make it 'personalized' per character.

--- src/AutoDrink.lua
+++ src/AutoDrink.lua
@@ -1,5 +1,6 @@
 
 -- AutoDrink by Jordon
+-- Personalized by OpusSF
 
 local AutoDrink = CreateFrame("Button", "AutoDrink", UIParent, "SecureActionButtonTemplate")
 AutoDrink:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
@@ -9,13 +10,19 @@ local AutoDrinks = {}
 
 AutoDrink:RegisterEvent("PLAYER_ENTERING_WORLD")
 function AutoDrink:PLAYER_ENTERING_WORLD()
-	if GetMacroIndexByName("AutoDrink") == 0 then
+	local macroIndex = GetMacroIndexByName("AutoDrink")  -- returns 0 or index of first macro that matches
+	if macroIndex == 0 then -- no macro found
+		DEFAULT_CHAT_FRAME:AddMessage("no macro found")
 		-- Max 36 macros per account
+		-- 36 is out of Date.  The first 120 macros are global, 121 starts the personal macros.
 		if GetNumMacros() == 36 then
 			DEFAULT_CHAT_FRAME:AddMessage("|cff33ff99AutoDrink|r: |cffff0000WARNING|r: Unable to create macro. Please free up a macro slot and reload your UI.")
 			return
 		end
-		CreateMacro("AutoDrink", "INV_MISC_QUESTIONMARK", "#showtooltip\n/AutoDrink Conjured Mana Fritter, Conjured Mana Pudding, Ley-Enriched Water, Highmountain Spring Water\n/click AutoDrink", nil)
+		createdIndex = CreateMacro("AutoDrink", "INV_MISC_QUESTIONMARK", "#showtooltip\n/AutoDrink Conjured Mana Fritter, Conjured Mana Pudding, Ley-Enriched Water, Highmountain Spring Water\n/click AutoDrink", 1)
+		-- CreateMacro("name", icon, "body", perCharacter, isLocal)
+	elseif macroIndex and macroIndex <= 120 then
+		DEFAULT_CHAT_FRAME:AddMessage("|cff33ff99AutoDrink|r: |cffff0000WARNING|r: There macro is set as GLOBAL. Please consider moving to perCharacter")
 	end
 	self:Fetch()
 end
